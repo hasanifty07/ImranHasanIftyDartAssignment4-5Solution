bf5ab286d738f2878e4326340bbf3899be04bd89
hasanifty07 <iftyand937@gmail.com>
2025-10-30 01:10:05 +0600

    Update name in file writing example

--- Assignment 5/q1.dart
+++ Assignment 5/q1.dart
@@ -1,8 +1,9 @@
 import "dart:io";
 void main() {
   String fileName = "hello.txt";
-  String myName = "soyoid safyan Faz";
+  String myName = "Imran Hasan Ifty";
   File file = File(fileName);
   file.writeAsStringSync(myName);
   print("Successfully wrote \"$myName\" to \"$fileName\".");
+
 }
